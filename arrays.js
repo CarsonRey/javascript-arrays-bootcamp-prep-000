94b74c60016eb9f131b7971bf3194fdc3c168aae
Carson Rey <c.rey494@gmail.com>
2018-08-15 18:54:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ function removeElementFromBeginningOfArray(array){
   array.slice(1); return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray() {
+function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
