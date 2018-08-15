0ff6380fdf947f41a54f5b8f6b2fd499c6b14a9b
Carson Rey <c.rey494@gmail.com>
2018-08-15 17:48:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var ingredient6 = "garlic"
 
 var chocolateBars = ["snickers","hundred grand", "kitcat", "skittles"];
 
-function addElementToBeginningOfArray() {
+function destructivelyAddElementToBeginningOfArray() {
   return chocolateBars.unshift("Skor bar");
 }
 
